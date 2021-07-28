1e60644c7c3c3f0c41e1977828499710ba0022c6
linus <linus.waldowsky@yahoo.de>
2021-07-28 14:28:48 +0200

    better binarization, fixed SIGSEGV

--- src/KinectTouch.cpp
+++ src/KinectTouch.cpp
@@ -14,11 +14,8 @@
  */
 
 #include <iostream>
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
 #include <ostream>
 #include <vector>
-#include <map>
 #include <fstream>
 #include <cmath>
 #include <list>
@@ -26,10 +23,13 @@
 using namespace std;
 
 // openCV
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/core/ocl.hpp>
 
 using namespace cv;
 
@@ -40,12 +40,6 @@ using namespace cv;
 #define CV_FILLED cv::FILLED
 
 // libfreenect2
-#include <libfreenect2/libfreenect2.hpp>
-#include <libfreenect2/frame_listener_impl.h>
-#include <libfreenect2/packet_pipeline.h>
-#include <libfreenect2/registration.h>
-#include <string>
-#include <iostream>
 
 // TUIO
 #include "TuioServer.h"
@@ -61,12 +55,26 @@ using namespace TUIO;
 //---------------------------------------------------------------------------
 // Globals
 //---------------------------------------------------------------------------
+void changeBlur(int, void*);
+void toggleFilter(int, void*);
+Mat calculateAverageFrame(list<Mat> *frames_, libfreenect2opencv::Libfreenect2OpenCV &libfreenect2OpenCV);
+void updateDistMatrix(int, void*);
+vector<Point2f> aggregateTouchPoints(vector<Point2f> &touchPoints, int radius);
+Mat createDistanceMatrix(int rows, int cols, Point2i originPoint, float minDistance, float pixelToMmConversionRatio);
+
+
 const  int framesForAverage = 40;
 list<Mat> frames;
 int oldestFrameIndex = 0;
 bool mousePressed = false;
 bool useFilter = false;
 Mat frameAcc = Mat::zeros(424, 512, CV_32FC1);
+const int frameHeight = 424;
+const int frameWidth = 512;
+const float mmPerPixel = 4.29;
+int threshFG = 1560;
+int threshBG = 1600;
+Mat distanceMat = createDistanceMatrix(frameHeight, frameWidth, Point(frameWidth / 2, frameHeight / 2), threshFG, mmPerPixel);
 
 //---------------------------------------------------------------------------
 // Functions
@@ -81,6 +89,7 @@ void changeBlur(int, void *strength) {
 
 void toggleFilter(int, void*) {
     useFilter = !useFilter;
+    cerr << "filter is: " << useFilter << endl;
 }
 
 Mat calculateAverageFrame(list<Mat> *frames_, libfreenect2opencv::Libfreenect2OpenCV &libfreenect2OpenCV) {
@@ -95,16 +104,48 @@ Mat calculateAverageFrame(list<Mat> *frames_, libfreenect2opencv::Libfreenect2Op
     return frameAcc / (double) frames_->size();
 }
 
-void test() {
-    Mat testMat(5, 5, CV_32FC1, 4.5f);
-    testMat.row(2).col(3) = 10;
-    testMat.row(3).col(2) = 2.3;
-    Mat res = (3 < testMat) & (5 > testMat);
-    cout << res << endl;
+void updateDistMatrix(int, void*) {
+    distanceMat = createDistanceMatrix(frameHeight, frameWidth, Point2i(frameWidth / 2, frameHeight / 2), threshFG, mmPerPixel);
+}
+
+vector<Point2f> aggregateTouchPoints(vector<Point2f> &touchPoints, int radius) {
+    vector<Point2f> aggregatedTouchPoints;
+    for (Point2f referenceTouchPoint : touchPoints) {
+        vector<Point2f> toBeAggregated;
+        toBeAggregated.push_back(referenceTouchPoint);
+        for (Point2f compareTouchPoint : touchPoints) {
+            if (distance_(referenceTouchPoint, compareTouchPoint) < radius) {
+                toBeAggregated.push_back(compareTouchPoint);
+            }
+        }
+
+        Point2f sum(0, 0);
+        for (Point2f touchPoint : toBeAggregated) {
+            sum += touchPoint;
+        }
+        aggregatedTouchPoints.push_back(sum / (float) toBeAggregated.size());
+    }
+    return aggregatedTouchPoints;
+}
+
+/**
+ * Creates a distance matrix for every point of the canvas/frame to the camera using pythagoras
+ * @param destination a reference to the matrix that is to be filled
+ * @param originPoint The point where the distance to the camera is minDistance, most likely the middle of the frame
+ * @param minDistance the distance from the originPoint to the camera
+ */
+Mat createDistanceMatrix(int rows, int cols, Point2i originPoint, float minDistance, float pixelToMmConversionRatio) {
+    Mat matrix = Mat::zeros(rows, cols, CV_32FC1);
+    for(int y = 0; y < rows; y++) {
+        for(int x = 0; x < cols; x++) {
+            float dist = distance_(originPoint, Point2i(x, y)) * pixelToMmConversionRatio;
+            matrix.row(y).col(x) = sqrt(pow(minDistance, 2) + pow(dist, 2));
+        }
+    }
+    return matrix;
 }
 
 int main() {
-
     Mat background;
     Mat touch;
     Mat binarized;
@@ -113,6 +154,8 @@ int main() {
     Mat depth;
     Mat mult;
     Mat contoursMat;
+    vector<Point2f> touchPoints;
+    int sendToTuio = 2; //send found touchpoints to tuio server every x loops.
     ShapeFilterStrategy *filter = new DensityFilter();
 //    ShapeFilterStrategy *filter = new ClusterFilter();
 
@@ -121,12 +164,10 @@ int main() {
     int debugthresh = 1;
     int blurStrength = 5;
     int blurStrength2 = 3;
-    int xMin = 0;
+    int xMin = 105;
     int xMax = 512;
-    int yMin = 46;
+    int yMin = 120;
     int yMax = 357;
-    int threshFG = 1800;
-    int threshBG = 1820;
     int filterRadius = 20;
 
     const bool localClientMode = true;                    // connect to a local client
@@ -141,10 +182,8 @@ int main() {
     TuioServer *tuio;
     if (localClientMode) {
         tuio = new TuioServer();
-        // std::cout << "--- local ---" << std::endl;
     } else {
         tuio = new TuioServer("192.168.0.2", 3333, false);
-        // std::cout << "--- NOOOOOT local ---" << std::endl;
     }
     TuioTime time;
     // }}}
@@ -152,7 +191,6 @@ int main() {
     // create some sliders {{{
     namedWindow(windowName);
     createTrackbar("blur strength", windowName, &blurStrength, 50, changeBlur, &blurStrength);
-//    createTrackbar("blur strength 2", windowName, &blurStrength2, 50, changeBlur, &blurStrength2);
     createTrackbar("threshFG", windowName, &threshFG, 2000);
     createTrackbar("threshBG", windowName, &threshBG, 2000);
     createTrackbar("xMin", windowName, &xMin, 512);
@@ -163,7 +201,9 @@ int main() {
     createTrackbar("touchMaxArea", windowName, &touchMaxArea, 10000);
     createTrackbar("FilterThreshold", windowName, filter->getDensityThresholdReference(), 50);
     createTrackbar("min shape size", windowName, filter->getMinShapeSizeReference(), 2000);
+    createTrackbar("filterRadius", windowName, &filterRadius, 100);
     createButton("toggleFilter", toggleFilter);
+    createButton("update Distance Matrix", updateDistMatrix);
     // }}}
 
     libfreenect2opencv::Libfreenect2OpenCV libfreenect2OpenCV;
@@ -176,36 +216,44 @@ int main() {
         frameAcc += tmpMat;
     }
 
-    int timeCount = 0, shotCount = 0;
     while ((char) waitKey(1) != (char) 27) {
         auto start = std::chrono::high_resolution_clock::now();
+//        updateDistMatrix(4, &distanceMat);
 
         depth = calculateAverageFrame(&frames, libfreenect2OpenCV);
-
-//        Mat detectionPlane = (threshFG > depth) & (threshBG < depth);
 
         Mat ir = libfreenect2OpenCV.getIRMat();
         Mat rgb = libfreenect2OpenCV.getRGBMat();
         cv::blur(depth, blur, Size(blurStrength, blurStrength));
-//        cv::threshold(blur, binarized, threshFG, 255, CV_8UC1);
-        binarized = (threshFG < blur) & (threshBG > blur);
-        bitwise_not(binarized, binarized);
-//        cv::blur(binarized, binarized, Size(blurStrength, blurStrength));
-//        cv::threshold(binarized, binarized, 1, 255, CV_8UC1);
-//        binarized.convertTo(binarized, CV_8UC1);
+
+//        binarized = (blur > threshFG) & (blur < threshBG);
+
+        binarized = Mat::zeros(blur.rows, blur.cols, CV_8UC1);
+
+        for(int y = 0; y < binarized.rows; y++) {
+            for(int x = 0; x < binarized.cols; x++) {
+                float value = blur.at<float>(y, x);
+                bool isRelevant = value > threshFG && value < threshBG;
+                if(!isRelevant) {
+                    binarized.at<uchar>(y,x) = 255;
+                }
+            }
+        }
+
+//        binarized = (distanceMat > blur);
+//        binarized = (threshFG > blur);
+//        bitwise_not(binarized, binarized);
 
         // extract ROI
         Rect roi(xMin, yMin, xMax - xMin, yMax - yMin);
         touchRoi = binarized(roi);
-        // cout << touchRoi << endl;
 
         // {{{ find touch points
         vector<vector<Point2i> > contours;
-        vector<Point2f> touchPoints;
+
         findContours(touchRoi, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, Point2i(xMin, yMin));
         vector<vector<Point> > hull(contours.size());
         for (unsigned int i = 0; i < contours.size(); i++) {
-            Mat contourMat(contours[i]);
             convexHull(Mat(contours[i]), hull[i], false);
         }
         // }}}
@@ -218,8 +266,6 @@ int main() {
         } else {
             filteredHulls = hull;
         }
-        //cout << "------------------------------------------------" << endl;
-        //cout << "Contours: " << contours.size() << endl << "Hulls: " << hull.size() << endl;
 
         for (vector<Point2i> hull : filteredHulls) {
             Point2i center = getCenter(hull);
@@ -227,10 +273,7 @@ int main() {
             touchPoints.push_back(touchPoint);
         }
 
-//        // Tracking
-//        Ptr<Tracker> tracker;
-//        tracker = TrackerMOSSE
-
+        touchPoints = aggregateTouchPoints(touchPoints, filterRadius);
 
         // {{{ send TUIO cursors
         time = TuioTime::getSessionTime();
@@ -239,15 +282,13 @@ int main() {
         for (unsigned int i = 0; i < touchPoints.size(); i++) { // touch points
             float cursorX = (touchPoints[i].x - xMin) / (xMax - xMin);
             float cursorY = 1 - (touchPoints[i].y - yMin) / (yMax - yMin);
-            // std::cout << "touchpoint[" << i << "]: x=" << cursorX << ", y=" << cursorY << std::endl;
             TuioCursor *cursor = tuio->getClosestTuioCursor(cursorX, cursorY);
+
             // TODO improve tracking (don't move cursors away, that might be closer to another touch point)
             if (cursor == NULL) {
                 tuio->addTuioCursor(cursorX, cursorY);
-                // std::cout << "addTuioCursor[" << i << "]: x=" << cursorX << ", y=" << cursorY << std::endl;
             } else {
                 tuio->updateTuioCursor(cursor, cursorX, cursorY);
-                // std::cout << "updateTuioCursor[" << i << "]: x=" << cursorX << ", y=" << cursorY << std::endl;
             }
         }
 
@@ -259,6 +300,7 @@ int main() {
         cvtColor(binarized, contoursMat, COLOR_GRAY2BGR);
 
         rectangle(contoursMat, roi, blue, 2); // surface boundaries
+        circle(contoursMat, Point2i(frameWidth/2, frameHeight/2), 8, magenta, -1);
 
         for (int i = 0; i < contours.size(); i++) {
             drawContours(contoursMat, contours, i, green);
@@ -269,20 +311,12 @@ int main() {
             circle(contoursMat, touchPoints[i], 3, red, 3);
         }
 
-//        for (Point2f touchPoint : touchPoints) {
-//            circle(contoursMat, touchPoint, 5, red, -1);
-//        }
+        for (Point2f touchPoint : touchPoints) {
+            circle(contoursMat, touchPoint, 5, red, -1);
+        }
 
         imshow(windowName, contoursMat);
-        imshow("avg depth", depth);
-
-//        Mat ir8;
-//        ir.convertTo(ir8, CV_8UC1);
-//        imshow("ir image", ir8);
-//
-//        imshow("rgb", rgb);
-
-//        imshow("depth debug", detectionPlane);
+//        imshow("avg depth", depth);
         // }}}
 
         auto stop = std::chrono::high_resolution_clock::now();
